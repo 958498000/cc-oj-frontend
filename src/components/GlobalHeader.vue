3bd23bc83ca7f9fe42f898e7f8eab8874bfbc21b
958498000 <958498000@qq.com>
2024-10-04 20:26:24 +0800

    根据权限显隐菜单

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,63 +1,89 @@
 <template>
-  <div>
-    <a-row id="globalHeader" align="center" :wrap="false">
-      <a-col flex="auto">
-        <a-menu
-          mode="horizontal"
-          :selected-keys="selectedKeys"
-          @menu-item-click="doMenuClick"
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <!-- :default-selected-keys="['1']" -->
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
         >
-          <a-menu-item
-            key="0"
-            :style="{ padding: 0, marginRight: '38px' }"
-            disabled
-          >
-            <div class="title-bar">
-              <img class="logo" src="@\assets\logo.png" />
-              <div class="title">ccoj</div>
-            </div>
-          </a-menu-item>
-          <a-menu-item v-for="item in routes" :key="item.path">{{
-            item.name
-          }}</a-menu-item>
-        </a-menu>
-      </a-col>
-      <a-col flex="100px">
-        <div>
-          {{ store.state.user?.loginUser?.userName ?? "未登录" }}
-        </div>
-      </a-col>
-    </a-row>
-  </div>
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" alt="" />
+            <div class="title">CCOJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        {{ store.state.user?.loginUser?.userName ?? "未登录" }}
+      </div>
+    </a-col>
+  </a-row>
 </template>
-<script setup lang="ts">
-import { useRouter, useRoute, routeLocationKey } from "vue-router";
-import { routes } from "../router/routes";
-import { ref } from "vue";
-import { useStore } from "vuex";
-const router = useRouter();
-const route = useRoute();
 
-//默认主页
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRoute, useRouter } from "vue-router";
+import { computed, ref, useSlots } from "vue";
+import { useStore } from "vuex";
+import checkAccess from "@/access/checkAccess";
+import ACCESS_ENUM from "@/access/accessEnum";
+
+const router = useRouter();
+const store = useStore();
+
+// 获取路由信息
+// const route = useRoute();
+
+// 展示在菜单的路由数组
+const visibleRoutes = computed(() => {
+  return routes.filter((item, index) => {
+    if (item.meta?.hideInMenu) {
+      return false;
+    }
+    // 根据权限过滤菜单
+    if (
+      !checkAccess(store.state.user.loginUser, item?.meta?.access as string)
+    ) {
+      return false;
+    }
+    return true;
+  });
+});
+
+// 默认主页
 const selectedKeys = ref(["/"]);
-//路由跳转后更新选中菜单
-router.afterEach((to, from, each) => {
+
+// 路由跳转时，更新选中的菜单项
+router.afterEach((to, from, failure) => {
   selectedKeys.value = [to.path];
 });
+
 const doMenuClick = (key: string) => {
   router.push({
     path: key,
   });
 };
-const store = useStore();
-console.log();
+
+console.log(store.state.user);
 setTimeout(() => {
   store.dispatch("user/getLoginUser", {
     userName: "西西",
-    role: "admin",
+    userRole: ACCESS_ENUM.ADMIN,
   });
 }, 3000);
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .title-bar {
   display: flex;
@@ -68,7 +94,6 @@ setTimeout(() => {
 }
 .title {
   color: #444;
-  margin-left: 10px;
-  font-weight: 700;
+  margin-left: 16px;
 }
 </style>
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,17 +1,19 @@
 import { routeLocationKey } from "vue-router";
 import { StoreOptions } from "vuex";
+import ACCESS_ENUM from "@/access/accessEnum";
 export default {
   namespaced: true,
   state: () => ({
     loginUser: {
       userName: "未登录",
-      role: "notLogin",
+      userRole: ACCESS_ENUM.NOT_LOGIN,
     },
   }),
+  getters: {},
   actions: {
     getLoginUser({ commit, state }, payload) {
       //  todo进行一些处理后提交 mutation 来更新状态
-      commit("updateUser", { userName: "西西" });
+      commit("updateUser", payload);
     },
   },
   mutations: {
