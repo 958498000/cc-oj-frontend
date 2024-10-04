359768430d499eb79df2043a9955cb9c4f8aaec3
958498000 <958498000@qq.com>
2024-10-04 20:37:38 +0800

    埋藏一个全局初始化函数

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,19 @@
 
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
+import { onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
-
+/**
+ * 全局初始化函数，有全局单次调用的代码都可以写到这里
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到我的项目");
+};
+//钩子函数
+onMounted(() => {
+  doInit();
+});
 const router = useRouter();
 const store = useStore();
 router.beforeEach((to, from, next) => {
