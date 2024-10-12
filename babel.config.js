cc4c511fa7f3864e1af4ab879d73a9ca87d73579
958498000 <958498000@qq.com>
2024-10-12 16:58:30 +0800

    题目创建,更新,修改模块完成

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: [
-    '@babel/plugin-transform-class-static-block'
-  ]
-
+  plugins: ["@babel/plugin-transform-class-static-block"],
 };
--- generated/models/Page_QuestionSubmitVO_.ts
+++ generated/models/Page_QuestionSubmitVO_.ts
@@ -1,19 +1,20 @@
-/* generated using openapi-typescript-codegen -- do not edit */
+/* generated using openapi-typescript-codegen -- do no edit */
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+
 import type { OrderItem } from './OrderItem';
-import type { Post } from './Post';
-export type Page_Post_ = {
+import type { QuestionSubmitVO } from './QuestionSubmitVO';
+
+export type Page_QuestionSubmitVO_ = {
     countId?: string;
     current?: number;
     maxLimit?: number;
     optimizeCountSql?: boolean;
     orders?: Array<OrderItem>;
     pages?: number;
-    records?: Array<Post>;
+    records?: Array<QuestionSubmitVO>;
     searchCount?: boolean;
     size?: number;
     total?: number;
 };
-
